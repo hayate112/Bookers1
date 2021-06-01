cc7e3c1db0f5f2b7e615127ae378eec3b0b847f0
hayate112 <k.hayate0112@gmail.com>
2021-06-01 12:26:14 +0000

    [Update]編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
